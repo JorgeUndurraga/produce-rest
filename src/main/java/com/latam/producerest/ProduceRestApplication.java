3c0c7db61fb1784d8774ed254651e36fd605865a
JorgeUndurraga <jorgeundurraga@yahoo.com>
2021-03-05 02:18:55 -0300

    Initial commit

---
+++ src/main/java/com/latam/producerest/ProduceRestApplication.java
@@ -0,0 +1,13 @@
+package com.latam.producerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduceRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/latam/producerest/ProduceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latam.producerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
